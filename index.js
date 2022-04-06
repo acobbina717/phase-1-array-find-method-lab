df36c7e5b68d47fa5f5128b8b9d7a81513897a9d
Abeku <acobbina717@gmail.com>
2022-04-06 09:37:45 -0400

    commit tree 418e4dfe239aa089ba320cf0f7e482a3ce7a969e

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    //...
+]
+
+// const sadReality = [
+//     { result: "N/A" },
+//     { result: "N/A" },
+//     { result: "N/A" }
+// ]
+
+function superbowlWin(array) {
+    const w = array.find(wins => wins.result === "W")
+    if (w) {
+        return w.year
+    } else {
+        return undefined
+    }
+}
+
+function findWins(wins) {
+    return wins.result === "W"
+}
+// function findYear(year) {
+//     return year.year === "W"
+// }
+superbowlWin(record)
